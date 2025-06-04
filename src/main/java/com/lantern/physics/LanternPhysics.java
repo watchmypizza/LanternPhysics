a039603da5766c5a909da8cf9cf64dcef809ea12
watchmypizza <kevinosswald@proton.me>
2025-06-04 19:33:21 +0200

    Added Comments, first commit. Testing

--- src/main/java/com/lantern/physics/LanternPhysics.java
+++ src/main/java/com/lantern/physics/LanternPhysics.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//Initialize the Mod on this stage
+
 public class LanternPhysics implements ModInitializer {
 	public static final String MOD_ID = "lanternphysics";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
--- src/main/java/com/lantern/physics/LanternPhysicsClient.java
+++ src/main/java/com/lantern/physics/LanternPhysicsClient.java
@@ -2,6 +2,8 @@ package com.lantern.physics;
 
 import net.fabricmc.api.ClientModInitializer;
 
+//Initialize the Mod for the Client
+
 public class LanternPhysicsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
