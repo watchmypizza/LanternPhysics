2b3ac1c298bba35b1b8407a76600b9cffb741bef
watchmypizza <kevinosswald@proton.me>
2025-06-04 19:28:51 +0200

    Hello GitHub!

---
+++ src/main/java/com/lantern/physics/LanternPhysics.java
@@ -0,0 +1,16 @@
+package com.lantern.physics;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LanternPhysics implements ModInitializer {
+	public static final String MOD_ID = "lanternphysics";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/lantern/physics/LanternPhysicsClient.java
@@ -0,0 +1,10 @@
+package com.lantern.physics;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LanternPhysicsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
